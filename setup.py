ccdd25be173e5a6213e86f8ccc9920dfbaefaa0c
segnolin <segnolin@gmail.com>
2016-06-07 20:12:52 +0800

    add build file
    Former-commit-id: a04e98f8cc7f6b7868e41e4e64ae4ed9db66be1a

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from cx_Freeze import setup, Executable
+
+import sys
+
+application_title = "Coda" #what you want to application to be called
+main_python_file = "main.py" #the name of the python file you use to run the program
+base = None
+
+if sys.platform == "win32":
+    
+    base = "Win32GUI"
+
+includes = ["atexit","re"]
+
+setup(
+        name = application_title,
+        version = "0.1",
+        description = "Visual Novel Game Engine",
+        options = {"build_exe" : {'includes': includes}},
+        executables = [Executable(main_python_file, base = base)])
