a6afd9eddf2642984bbd4d570327c905cf02c6ec
segnolin <segnolin@gmail.com>
2016-06-28 20:21:11 +0800

    use paintEvent
    Former-commit-id: ae3ba441dc625594a175cc0b7a73abda6ab8182b

--- image_button_class.py
+++ image_button_class.py
@@ -8,51 +8,34 @@ from PyQt5.QtCore import *
 import sys
 import resources
 
-class ImageButton(QPushButton):
+class ImageButton(QAbstractButton):
     '''this class provide image button functions'''
 
-    #constructor
     def __init__(self, button_id, parent):
-        super().__init__(parent) #call super class constructor
+        super().__init__(parent)
 
-        self.id = button_id #get button name from positional argument
+        self.id = button_id
 
-        self.create_button()
+        self.pressed.connect(self.update)
+        self.released.connect(self.update)
 
-    def create_button(self):
+        self.pixmap_defaults = QPixmap(':/{0}_button_defaults.png'.format(self.id))
+        self.pixmap_hover = QPixmap(':/{0}_button_hover.png'.format(self.id))
+        self.pixmap_press = QPixmap(':/{0}_button_press.png'.format(self.id))
 
-        #set defaults image of button
-        self.palette_defaults = QPalette()
-        self.palette_defaults.setBrush(self.backgroundRole(), QBrush(QPixmap(':/{0}_button_defaults.png'.format(self.id))))
+    def paintEvent(self, event):
 
-        #set hover image of button
-        self.palette_hover = QPalette()
-        self.palette_hover.setBrush(self.backgroundRole(), QBrush(QPixmap(':/{0}_button_hover.png'.format(self.id))))
+        pixmap = self.pixmap_hover if self.underMouse() else self.pixmap_defaults
+        if self.isDown():
+            pixmap = self.pixmap_press
 
-        #set press image of button
-        self.palette_press = QPalette()
-        self.palette_press.setBrush(self.backgroundRole(), QBrush(QPixmap(':/{0}_button_press.png'.format(self.id))))
+        painter = QPainter(self)
+        painter.drawPixmap(event.rect(), pixmap)
 
-        self.setAutoFillBackground(True)
-        self.setPalette(self.palette_defaults)
-        self.installEventFilter(self) #call event filter function
+    def enterEvent(self, event):
 
-    def eventFilter(self, object, event):
+        self.update()
 
-        if event.type() == QEvent.HoverMove:
+    def leaveEvent(self, event):
 
-            self.setPalette(self.palette_hover)
-
-        elif event.type() == QEvent.MouseButtonPress:
-
-            self.setPalette(self.palette_press)
-
-        elif event.type() == QEvent.HoverLeave:
-
-            self.setPalette(self.palette_defaults)
-
-        elif event.type() == QEvent.Paint:
-
-            return True
-
-        return False
+        self.update()
