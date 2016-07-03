dbd063dce0d554421316c4edebab3da8ceb37ceb
segnolin <segnolin@gmail.com>
2016-07-03 11:06:17 +0800

    update parser function
    Former-commit-id: 94006984411de60a5501d6f646b46f86df540082

--- parser.py
+++ parser.py
@@ -15,23 +15,36 @@ class Parser(QXmlStreamReader):
 
         self.id = str(game_engine_id)
 
-        self.bgm = ''
-        self.sd = ''
-        self.bg = ''
-        self.ptid = {}
-        self.ptx = {}
-        self.pty = {}
-        self.ptxf = {}
-        self.ptyf = {}
-        self.ptw = {}
-        self.pth = {}
-        self.vc = ''
-        self.char = ''
-        self.txt = ''
+        self.bgm_id = ''
+
+        self.sd_id = ''
+
+        self.eff_id = ''
+        self.eff_du = ''
+
+        self.bg_id = ''
+        self.bg_x = ''
+        self.bg_y = ''
+        self.bg_xf = ''
+        self.bg_yf = ''
+        self.bg_du = ''
+
+        self.pt_id = {}
+        self.pt_x = {}
+        self.pt_y = {}
+        self.pt_xf = {}
+        self.pt_yf = {}
+
+        self.tb_sh = ''
+        self.tb_td = ''
+        self.tb_vc = ''
+        self.tb_char = ''
+        self.tb_txt = ''
+        self.tb_hi = ''
 
         self.pt_num = -1
 
-        self.file = QFile(':/script.xml')
+        self.file = QFile(':/totono.xml')
         self.file.open(QIODevice.ReadOnly)
         self.setDevice(self.file)
 
@@ -52,16 +65,14 @@ class Parser(QXmlStreamReader):
                     self.parse_bgm()
                 elif self.name() == 'sd':
                     self.parse_sd()
+                elif self.name() == 'eff':
+                    self.parse_eff()
                 elif self.name() == 'bg':
                     self.parse_bg()
                 elif self.name() == 'pt':
                     self.parse_pt()
-                elif self.name() == 'vc':
-                    self.parse_vc()
-                elif self.name() == 'char':
-                    self.parse_char()
-                elif self.name() == 'txt':
-                    self.parse_txt()
+                elif self.name() == 'tb':
+                    self.parse_tb()
                 else:
                     self.readNext()
 
@@ -72,87 +83,144 @@ class Parser(QXmlStreamReader):
 
     def parse_bgm(self):
 
-        self.bgm = self.readElementText()
-        #print(self.bgm)
+         while not self.atEnd():
+            self.readNext()
+            if self.isStartElement():
+                if self.name() == 'id':
+                    self.bgm_id = self.readElementText()
+                else:
+                    self.readNext()
+
+            if self.isEndElement():
+                if self.name() == 'bgm':
+                    break
 
     def parse_sd(self):
 
-        self.sd = self.readElementText()
-        #print(self.sd)
+         while not self.atEnd():
+            self.readNext()
+            if self.isStartElement():
+                if self.name() == 'id':
+                    self.sd_id = self.readElementText()
+                else:
+                    self.readNext()
 
-    def parse_bg(self):
+            if self.isEndElement():
+                if self.name() == 'sd':
+                    break
 
-        self.bg = self.readElementText()
-        #print(self.bg)
-
-    def parse_pt(self):
-
-        self.pt_num += 1
-        #print(self.pt_num)
+    def parse_eff(self):
 
         while not self.atEnd():
             self.readNext()
             if self.isStartElement():
-                if self.name() == 'ptid':
-                    self.ptid[self.pt_num] = self.readElementText()
-                    #print(self.ptid[self.pt_num])
-                elif self.name() == 'ptx':
-                    self.ptx[self.pt_num] = self.readElementText()
-                    #print(self.ptx[self.pt_num])
-                elif self.name() == 'pty':
-                    self.pty[self.pt_num] = self.readElementText()
-                    #print(self.pty[self.pt_num])
-                elif self.name() == 'ptxf':
-                    self.ptxf[self.pt_num] = self.readElementText()
-                    #print(self.ptxf[self.pt_num])
-                elif self.name() == 'ptyf':
-                    self.ptyf[self.pt_num] = self.readElementText()
-                    #print(self.ptyf[self.pt_num])
-                elif self.name() == 'ptw':
-                    self.ptw[self.pt_num] = self.readElementText()
-                    #print(self.ptw[self.pt_num])
-                elif self.name() == 'pth':
-                    self.pth[self.pt_num] = self.readElementText()
-                    #print(self.pth[self.pt_num])
+                if self.name() == 'id':
+                    self.eff_id = self.readElementText()
+                elif self.name() == 'du':
+                    self.eff_du = self.readElementText()
+                else:
+                    self.readNext()
+
+            if self.isEndElement():
+                if self.name() == 'eff':
+                    break
+
+    def parse_bg(self):
+
+        while not self.atEnd():
+            self.readNext()
+            if self.isStartElement():
+                if self.name() == 'id':
+                    self.bg_id = self.readElementText()
+                elif self.name() == 'x':
+                    self.bg_x = self.readElementText()
+                elif self.name() == 'y':
+                    self.bg_y = self.readElementText()
+                elif self.name() == 'xf':
+                    self.bg_xf = self.readElementText()
+                elif self.name() == 'yf':
+                    self.bg_yf = self.readElementText()
+                elif self.name() == 'du':
+                    self.bg_du = self.readElementText()
+                else:
+                    self.readNext()
+
+            if self.isEndElement():
+                if self.name() == 'bg':
+                    break
+
+    def parse_pt(self):
+
+        self.pt_num += 1
+
+        while not self.atEnd():
+            self.readNext()
+            if self.isStartElement():
+                if self.name() == 'id':
+                    self.pt_id[self.pt_num] = self.readElementText()
+                elif self.name() == 'x':
+                    self.pt_x[self.pt_num] = self.readElementText()
+                elif self.name() == 'y':
+                    self.pt_y[self.pt_num] = self.readElementText()
+                elif self.name() == 'xf':
+                    self.pt_xf[self.pt_num] = self.readElementText()
+                elif self.name() == 'yf':
+                    self.pt_yf[self.pt_num] = self.readElementText()
                 else:
                     self.readNext()
 
             if self.isEndElement():
                 if self.name() == 'pt':
-                    #print('break pt')
                     break
 
-    def parse_vc(self):
+    def parse_tb(self):
 
-        self.vc = self.readElementText()
-        #print(self.vc)
+        while not self.atEnd():
+            self.readNext()
+            if self.isStartElement():
+                if self.name() == 'sh':
+                    self.tb_sh = self.readElementText()
+                elif self.name() == 'td':
+                    self.tb_td = self.readElementText()
+                elif self.name() == 'vc':
+                    self.tb_vc = self.readElementText()
+                elif self.name() == 'char':
+                    self.tb_char = self.readElementText()
+                elif self.name() == 'txt':
+                    self.tb_txt = self.readElementText()
+                elif self.name() == 'hi':
+                    self.tb_hi = self.readElementText()
+                else:
+                    self.readNext()
 
-    def parse_char(self):
-
-        self.char = self.readElementText()
-        #print(self.char)
-
-    def parse_txt(self):
-
-        self.txt = self.readElementText()
-        #print(self.txt)
+            if self.isEndElement():
+                if self.name() == 'tb':
+                    break
 
 if __name__ == '__main__':
 
-    game_engine_id = 2
+    game_engine_id = 10
     parser = Parser(game_engine_id)
 
-    print(parser.bgm)
-    print(parser.sd)
-    print(parser.bg)
-    print(parser.ptid)
-    print(parser.ptx)
-    print(parser.pty)
-    print(parser.ptxf)
-    print(parser.ptyf)
-    print(parser.ptw)
-    print(parser.pth)
-    print(parser.vc)
-    print(parser.char)
-    print(parser.txt)
+    print(parser.bgm_id)
+    print(parser.sd_id)
+    print(parser.eff_id)
+    print(parser.eff_du)
+    print(parser.bg_id)
+    print(parser.bg_x)
+    print(parser.bg_y)
+    print(parser.bg_xf)
+    print(parser.bg_yf)
+    print(parser.bg_du)
+    print(parser.pt_id)
+    print(parser.pt_x)
+    print(parser.pt_y)
+    print(parser.pt_xf)
+    print(parser.pt_yf)
+    print(parser.tb_sh)
+    print(parser.tb_td)
+    print(parser.tb_vc)
+    print(parser.tb_char)
+    print(parser.tb_txt)
+    print(parser.tb_hi)
     print(parser.pt_num)
