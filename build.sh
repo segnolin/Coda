6113ea2b9a8486232bf4efc594be27979edbe489
segnolin <segnolin@gmail.com>
2017-02-03 12:31:15 +0800

    adjust build system
    Former-commit-id: 4b74dbd00d0337524dd771233eea02325e1f0fa5

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "pyinstaller" ]; then
-    mkdir pyinstaller
-    git clone https://github.com/pyinstaller/pyinstaller.git pyinstaller
-fi
-
-python3 pyinstaller/pyinstaller.py main.spec
